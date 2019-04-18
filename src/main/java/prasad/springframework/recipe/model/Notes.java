96ae8214cd34aa5bfeffc88b4ff837bc376d8b8b
hekenderi <hemprasadk@gmail.com>
2019-04-18 07:55:02 -0400

    intial upload

---
+++ src/main/java/prasad/springframework/recipe/model/Notes.java
@@ -0,0 +1,43 @@
+package prasad.springframework.recipe.model;
+
+import javax.persistence.*;
+
+@Entity
+public class Notes {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Recipe recipe;
+
+    @Lob
+    private String recipenotes;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+
+
+    public Recipe getRecipe() {
+        return recipe;
+    }
+
+    public void setRecipe(Recipe recipe) {
+        this.recipe = recipe;
+    }
+
+    public String getRecipenotes() {
+        return recipenotes;
+    }
+
+    public void setRecipenotes(String recipenotes) {
+        this.recipenotes = recipenotes;
+    }
+
+}
---
+++ src/main/java/prasad/springframework/recipe/model/Recipe.java
@@ -0,0 +1,113 @@
+package prasad.springframework.recipe.model;
+
+import org.hibernate.engine.internal.Cascade;
+import org.springframework.stereotype.Service;
+
+import javax.persistence.*;
+
+@Entity
+public class Recipe {
+
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String description;
+    private String prepTime;
+    private String cookTime;
+    private String servings;
+    private String source;
+
+    private String url;
+    private String direction;
+    //private Difficulity difficulity;
+
+    @Lob
+    private Byte[] picture;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    private Notes notes;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getPrepTime() {
+        return prepTime;
+    }
+
+    public void setPrepTime(String prepTime) {
+        this.prepTime = prepTime;
+    }
+
+    public String getCookTime() {
+        return cookTime;
+    }
+
+    public void setCookTime(String cookTime) {
+        this.cookTime = cookTime;
+    }
+
+    public String getServings() {
+        return servings;
+    }
+
+    public void setServings(String servings) {
+        this.servings = servings;
+    }
+
+    public String getSource() {
+        return source;
+    }
+
+    public void setSource(String source) {
+        this.source = source;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getDirection() {
+        return direction;
+    }
+
+    public void setDirection(String direction) {
+        this.direction = direction;
+    }
+
+    public Byte[] getPicture() {
+        return picture;
+    }
+
+    public void setPicture(Byte[] picture) {
+        this.picture = picture;
+    }
+
+    public Notes getNotes() {
+        return notes;
+    }
+
+    public void setNotes(Notes notes) {
+        this.notes = notes;
+    }
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+
+}
